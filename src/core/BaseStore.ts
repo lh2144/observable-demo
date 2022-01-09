4853b75099d0e48642cf62cca88523c42aa7d0a9
连航 <cangqiong.lian@tuya.com>
2022-01-09 23:02:10 +0800

    feat: 修改setstate 逻辑

--- src/core/BaseStore.ts
+++ src/core/BaseStore.ts
@@ -1,44 +1,69 @@
-import { Observable } from "rxjs"
-import { createState } from "./utils/createState"
+import { Observable } from 'rxjs';
+import { createState } from './utils/createState';
 
 export interface StateResult<T> {
-  state$: Observable<T>
-  getState(): T
-  setState(val: any): void
+  state$: Observable<T>;
+  getState(): T;
+  setState(val: any): void;
 }
+
+type StateFunc<T> = (state: T) => Partial<T>;
 interface Config {
-  stateHistory: boolean
-  logState: boolean
+  stateHistory?: boolean;
+  logState?: boolean;
 }
 
 export class BaseStore<s> {
+  private state: StateResult<s>;
 
-  private state: StateResult<s>
-
-  constructor(defaultState: s, setting: Config) {
-    this.state = createState(defaultState)
-    this.stateChange = this.state.state$
+  constructor(defaultState: s, private setting: Config = { logState: true }) {
+    this.state = createState(defaultState);
+    this.stateChange = this.state.state$;
   }
+  public stateChange: Observable<s>;
 
-  public setState(val: Partial<s>) {
-    this.state.setState(val)
-  }
-
-  public stateChange: Observable<s>
-
-  public getState(selector?: (state: s) => any): s | Partial<s> | any {
-    const state = this.state.getState()
-    if (selector) {
-      return selector(state)
+  protected setState(val: Partial<s> | StateFunc<s>, action?: any) {
+    const preState = this.getAllState();
+    switch (typeof val) {
+      case 'object':
+        this.state.setState(val);
+        break;
+      case 'function':
+        const newState = val(preState);
+        this.state.setState(newState);
+        break;
+      default:
+        throw Error('Please pass an object or function to set state');
     }
-    return state
+
+    if (this.setting.logState) {
+      const caller = this.constructor
+        ? '\r\nCaller: ' + this.constructor.name
+        : '';
+      console.log(
+        '%cSTATE CHANGED',
+        'font-weight: bold',
+        '\r\nAction: ',
+        action,
+        caller,
+        '\r\nState: ',
+        val
+      );
+    }
   }
 
-  public getAllState(): s {
-    return this.state.getState()
+  protected getState(selector?: (state: s) => any): s | Partial<s> | any {
+    const state = this.state.getState();
+    if (selector) {
+      return selector(state);
+    }
+    return state;
+  }
+
+  protected getAllState(): s {
+    return this.state.getState();
   }
   // public getState$() {
   //   return this.state.state$
   // }
-
 }
